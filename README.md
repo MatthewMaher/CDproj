562379e4e9db9f7974808bf926220e9da3e12a0f
Matthew <matthew.jl.maher\@gmail.com>
2015-02-17 12:08:46 -0500

    Class Project Assignment

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# CDproj
-Class Project for Cleaning Data Course
+# Overview Purpose
+This Repository contains a tool for producing a summarization of data from:
+
+**Version 1.0 of Human Activity Recognition Using Smartphones Dataset**
+
+
+# Input Data Source and Format
+For that original data set:
+
+   the raw data can viewed at:  
+      https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
+   Descriptions of the original source data are at:
+      http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+
+
+# Output Data Source and Format Descriptions
+The summarized data set provides the mean value, for unique combinations of SUBJECT and ACTIVITY,
+of a subset of measurement values (those representing a Mean or an Std Dev).  
+
+The measurements that were summarized were those original dataset 'features' with names ended in 'mean' or 'std'
+
+The Output Dataset contains: 
+
+ * A merged view of the Testing and Training Data (which are separate in the input dataset)
+ 
+ * explicit acitivty descriptions rather than numeric code (as in original input)
+
+ * One row per each unique combination of SUBJECT and ACITIVITY (180 rows)
+ 
+ * One data column for each measurement that is summarized  (66 measurements)
+ 
+ * One row of column headings at the top
+
+# Execution 
+
+The entire procedure for constructing the data summarization is within the script file:  **run_analysis.R**, which is internally documented on a step by step basis. 
+
+You should first download and locally expand ZIP file source data (URL above).
+
+The single contained function, "**run_analysis()**" has two optional parameters:
+
+* the directory in which the original data set tree (from URL above is located).  default = '.'
+
+* the output filename into which the data should be written.  default = './tidy.dat'
+
+Besides the creation of a tidy.dat file, the summarized data are also returned as a data.frame from the function.  
+
+# Sample Demo
+
+```
+> setwd("~/CDproj")
+> source("run_analysis.R")
+> tidy <- run_analysis()
+writing results to file: ./tidy.dat...
+> dim(tidy)
+[1] 180  68
+```
---
+++ run_analysis.R
@@ -0,0 +1,59 @@
+run_analysis <- function(directory='.', outfile='./tidy.dat') {
+  
+  # Load up the list of Activities and their numerical codes
+  act_file <- sprintf("%s/activity_labels.txt", directory)
+  activity_list <- read.table(act_file, col.names = c('Actnum','Activity'))
+  
+  # Load up the list of Features that are recorded in the columns of the input data
+  feat_file <- sprintf("%s/features.txt", directory)
+  features <- read.table(feat_file, col.names = c('Featnum','Feature'))
+  
+  # we are only interested in those features that are a Mean or STD value
+  features_to_keep <- grepl('-(std|mean)\\(',features$Feature)
+  
+  # there are two directories of data to combine.
+  sets <- c('test','train')
+  combined <- NULL
+  for (d in sets) {
+    
+       # in each directory there are three parallel files: subjects, activities, and data
+       # we need to read them in separately and then glue the columns together with a cbind()
+    
+       sub_file <- sprintf("%s/%s/subject_%s.txt", directory, d, d)
+       subjects <- read.table(sub_file, col.names = c('subject'))
+       
+       act_file <- sprintf("%s/%s/y_%s.txt", directory, d, d)
+       activities <- read.table(act_file, col.names = c('Activity'))
+       
+       dat_file <- sprintf("%s/%s/X_%s.txt", directory, d, d)
+       data <- read.table(dat_file)    
+       
+       dframe <- cbind(subjects, activities, data)
+       
+       # the 'combined' variable will hold the appended versions of both directories
+       if (is.null(combined)) {
+           combined <- dframe
+       } else {
+         combined = rbind(combined, dframe)
+       }
+  } 
+  
+  # make a new version of the results that removes all the unwanted columns
+  filtered_result <- combined[c(TRUE, TRUE, features_to_keep)]
+  
+  # apply real column names based on the names from the features.txt file
+  data_columns <- sub("\\(\\)", "", features$Feature[features_to_keep]) 
+  colnames(filtered_result) = c('subject', 'Activity', data_columns )
+  
+  # replace the Activity numeric codes with the real names (from activity_lables.txt)
+  filtered_result$Activity <- activity_list$Activity[filtered_result$Activity]
+  
+  # Now summarize by the Subject and Acitivity to get to a tidy version
+  tidy_result <-aggregate(.~subject+Activity, filtered_result, FUN=mean)
+ 
+  # write out the results to a file and return as a function response
+  message("writing results to file: ", outfile, "...")
+  write.table(tidy_result, file=outfile, row.names=FALSE)
+  tidy_result
+}
+
